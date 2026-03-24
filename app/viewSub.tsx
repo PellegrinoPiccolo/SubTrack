bf92f0c66886dbac4b12f6f165a6ab6f3cd86948
Pellegrino Piccolo <piccolopellegrino22@gmail.com>
2026-03-24 09:39:39 +0100

    fix: reduce vertical padding in header style for improved layout

--- app/viewSub.tsx
+++ app/viewSub.tsx
@@ -511,7 +511,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingHorizontal: 16,
-    paddingVertical: 16,
+    paddingVertical: 10,
     borderBottomWidth: 1,
   },
   headerButton: {
